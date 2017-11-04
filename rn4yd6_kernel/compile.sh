0a7b00f86ad39bf6e464a3637d08085b66c006f7
okest <792185968@qq.com>
2017-11-04 08:24:02 +0800

    submit compile sh

---
+++ rn4yd6_kernel/compile.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+echo "step : 1  ready to config"
+make  atlas7_rn4yd6_defconfig
+
+echo "step : 2  ready to make dtb"
+make atlas7-rn4yd6.dtb
+
+echo "step : 3  make zImage -j4"
+make -j4
+
+
+echo ".....done......"
+
+echo "-----GPL-----"
+echo "-----AUTHOR-----okest-----"
+echo "-----AUTHOR-----koberx-----"
+echo "------THANKS-----"
+
+sleep 1s
+echo "copy to /mnt/share"
+
+cp arch/arm/boot/dts/atlas7-rn4yd6.dtb   /mnt/share
+cp arch/arm/boot/zImage  /mnt/share
+
